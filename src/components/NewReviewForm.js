2c3fd69300b357190b813927b163fc90b9019def
Josh Feldman <joshfeldman500@gmail.com>
2023-04-24 13:01:41 -0400

    monday 1pm

--- src/components/NewReviewForm.js
+++ src/components/NewReviewForm.js
@@ -1,9 +1,9 @@
 import React, {useEffect, useState} from "react";
 const apiKey="67a39b6d0acb87e2c654ffb8e3194f5828d48b6fc98874795140feabfbfb9f196b5fd846066cb69fefc812cba8a23879"
-const gpsCode = "OTHH"
 
 function NewReviewForm() {
   const [airport, setAirport] = useState({})
+  const [gpsCode, setGpsCode] = useState("")
   function handleReviewSubmit(e){
     e.preventDefault()
   }
@@ -24,17 +24,29 @@ function NewReviewForm() {
   // let municipality = airport.municipality
   // let country = airport.country.name
  // }
-
+// const handleReviewSubmit = (e)=>{
+//   const newReview = `${e.target.value}`
+// }
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
       <form onSubmit={handleReviewSubmit}>
-      <datalist id="mylist">
-        <option value = {`${airport?.iata_code} - ${airport?.name} - ${airport?.municipality}, ${airport?.country?.name}`} />
-      </datalist>
-        <input type="search" name="departure" list="mylist" placeholder="Departure Airport" />
-        <input type="search" name="arrival" placeholder="Arrival Airport" />
-        <input type="date" name="date"  placeholder="Date" />
+        <select onChange={(e)=> setGpsCode(e.target.value)}>
+          <option>Select Departure</option>
+          <option value = {`KJFK`} >JFK</option>
+          <option value = {`OTHH`} >DOH</option>
+        </select>
+        {/* <input type="search" name="departure" list="mylist" placeholder="Departure Airport" /> */}
+        <select onChange={(e)=> setGpsCode(e.target.value)}>
+          <option>Select Departure</option>
+          <option value = {`KJFK`} >JFK</option>
+          <option value = {`OTHH`} >DOH</option>
+        </select>
+        <input type="date" id="date"  placeholder="Date" />
+        <a/>
+          <textarea id="review" rows="4" cols="50" placeholder="Share your experience!">
+       
+        </textarea>
         <button type="submit">Submit</button>
       </form>
     </div>
