1c9929cac89c0452d57e4d567f45b720c4909c87
Josh Feldman <joshfeldman500@gmail.com>
2023-04-24 10:23:22 -0400

    fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
+const apiKey="67a39b6d0acb87e2c654ffb8e3194f5828d48b6fc98874795140feabfbfb9f196b5fd846066cb69fefc812cba8a23879"
+const gpsCode = "OTHH"
+// const url="https://api.api-ninjas.com/v1/airports"
+// let options = {
+//   method: 'GET',
+//   headers: { 'x-api-key': "oXBEJfC9mYEY8R25nkiXZQ==T1FRszwualM66Hnt" }
+// }
+
+
 
 function App() {
+  //const name = 'London Heathrow'
+
+  useEffect(() => {
+    fetch(`https://airportdb.io/api/v1/airport/${gpsCode}?apiToken=${apiKey}`, {
+      headers: {"Content-Type": 'application/json'},
+  })
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, []);
+    
+
+//     method: 'GET',
+//     url: 'https://api.api-ninjas.com/v1/airports?name=' + name,
+//     headers: { 'X-Api-Key': 'YOUR_API_KEY'},
+//     contentType: 'application/json',
+//     success: function(result) {
+//         console.log(result);
+//     },
+//     error: function ajaxError(jqXHR) {
+//         console.error('Error: ', jqXHR.responseText);
+//     }
+// });
+//   }, []);
+
+
+
   return (
     <div className="app">
       <Header />
