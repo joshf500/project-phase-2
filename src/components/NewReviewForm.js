07e3bac44c6625fec1dcc0f4cc9a766316a5bdf6
Josh Feldman <joshfeldman500@gmail.com>
2023-04-24 12:02:55 -0400

    form search

--- src/components/NewReviewForm.js
+++ src/components/NewReviewForm.js
@@ -1,36 +1,36 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 const apiKey="67a39b6d0acb87e2c654ffb8e3194f5828d48b6fc98874795140feabfbfb9f196b5fd846066cb69fefc812cba8a23879"
 const gpsCode = "OTHH"
 
-function NewReviewForm({onReviewSubmit}) {
-  
+function NewReviewForm() {
+  const [airport, setAirport] = useState({})
   function handleReviewSubmit(e){
     e.preventDefault()
   }
 
   useEffect(() => {
-    fetch(`https://airportdb.io/api/v1/airport/${gpsCode}?apiToken=${apiKey}`, {
-      headers: {"Content-Type": 'application/json'},
-  })
+    fetch(`https://airportdb.io/api/v1/airport/${gpsCode}?apiToken=${apiKey}`)
       .then(res => res.json())
-      .then(data => onReviewSubmit(data))
-      
+      .then(data => {
+        setAirport(data)
+        console.log(data)
+       })
   }, []);
-  console.log(data)
+ 
   
-  // const airportOption = (data) => {
-  // const code = data.iata_code
-  // const name = data.name
-  // const municipality = data.municipality
-  // const country = data.country.name
-  // }
+  //const airportOption = (data) => {
+  // let code = airport.iata_code
+  // let name = airport.name
+  // let municipality = airport.municipality
+  // let country = airport.country.name
+ // }
 
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
       <form onSubmit={handleReviewSubmit}>
       <datalist id="mylist">
-        <option value = {`${data.iata_code} - ${data.name} - ${data.municipality}, ${data.name.country}`} />
+        <option value = {`${airport?.iata_code} - ${airport?.name} - ${airport?.municipality}, ${airport?.country?.name}`} />
       </datalist>
         <input type="search" name="departure" list="mylist" placeholder="Departure Airport" />
         <input type="search" name="arrival" placeholder="Arrival Airport" />
