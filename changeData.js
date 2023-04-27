056ef5c2f3ccbce0fb6bcea9d4e05010400eb063
Dennis Farley <dfarley1@binghamton.edu>
2023-04-27 17:12:48 -0400

    Add faker gem and add review data to db

--- changeData.js
+++ changeData.js
@@ -1,11 +1,27 @@
-var fs = require('fs');
+const { faker } = require('@faker-js/faker');
+let fs = require('fs');
 const airlines = require('./airlines.json');
 
 function change(data){
     let idCounter = 1
 
     let newAirlineData = data.airlines.map(airline => {
-        airline.reviews = []
+        const reviews = []
+        for(let i = 0; i < (Math.random() * 20); i++){
+            const randomNum = Math.random() * 20
+            const date = faker.date.between("2018-01-01", "2023-04-28").toISOString().split("T")[0];
+            reviews.push({
+                id: i + 1,
+                "depAirport": "DOH",
+                "arrAirport": "JFK",
+                airline: airline.name,
+                date,
+                title: faker.lorem.sentence(),
+                text: faker.lorem.lines(randomNum),
+            })
+        }
+
+        airline.reviews = reviews
 
         let appId = airline.id
         airline.appId = appId
