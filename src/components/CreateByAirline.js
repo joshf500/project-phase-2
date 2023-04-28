a14e95fb08dee6f671e60b6ec1a2807d30edef7a
+ <+>
2023-04-28 10:00:44 -0400

    adjust component

--- src/components/CreateByAirline.js
+++ src/components/CreateByAirline.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {useEffect,useState} from "react";
-
+import {useNavigate} from 'react-router-dom'
 import Search from "./Search";
-import CreateAirlineList from "./CreateAirlineList";
+// import CreateAirlineList from "./CreateAirlineList";
 
 export default function CreateByAirline(){
     const [airlines, setAirlines] = useState([])
     const [searchInput, setSearchInput] = useState("");
-
+    const navigate = useNavigate()
     useEffect(() => {
         fetch(`http://localhost:6001/airlines`)
           .then(res => res.json())
@@ -23,13 +23,30 @@ export default function CreateByAirline(){
         // setAirlines(airlines);
       }
 
-      const filteredAirlines = airlines.filter((airline) => {
-        return airline.name.toLowerCase().includes(searchInput.toLowerCase());
-      });
+      // const filteredAirlines = airlines.filter((airline) => {
+      //   return airline.name.toLowerCase().includes(searchInput.toLowerCase());
+      // });
       //console.log(filteredAirlines)
 
       //const filteredlist = setAirlines(filteredAirlines)
-
+      function CreateAirlineList({airlines}){
+        return (
+            <ul>
+        {airlines.map((airline) => {
+            console.log(airline.logo);
+        return(
+        <span class="options" key={airline.id} onClick={()=>{navigate('/newreviewform',{state:{selected: airline.name}});}} >
+            <img src={airline.logo}/>
+            <p>{airline.name}</p>
+        </span>   
+        )
+        })}
+          </ul>
+        )
+      }
+       const filteredAirlines = airlines.filter((airline) => {
+        return airline.name.toLowerCase().includes(searchInput.toLowerCase());
+      });
     return(
         <>
             <main>
