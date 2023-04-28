4d1c7219e39885c23ef5d31c69d81c2645c1b851
+ <+>
2023-04-28 13:23:49 -0400

    adjust data

--- changeData.js
+++ changeData.js
@@ -2,25 +2,27 @@ const { faker } = require('@faker-js/faker');
 let fs = require('fs');
 const airlines = require('./airlines.json');
 
+
 function change(data){
     let idCounter = 1
 
     let newAirlineData = data.airlines.map(airline => {
         const reviews = []
-        for(let i = 0; i < (Math.random() * 20); i++){
-            const randomNum = Math.random() * 20
-            const date = faker.date.between("2018-01-01", "2023-04-28").toISOString().split("T")[0];
-            reviews.push({
-                id: i + 1,
-                "depAirport": "DOH",
-                "arrAirport": "JFK",
-                airline: airline.name,
-                date,
-                title: faker.lorem.sentence(),
-                text: faker.lorem.lines(randomNum),
-                rating: Math.floor(Math.random() * 6)
-            })
-        }
+        // for(let i = 0; i < (Math.random() * 20); i++){
+        //     const randomNum = Math.random() * 20
+        //     const date = faker.date.between("2018-01-01", "2023-04-28").toISOString().split("T")[0];
+        //     reviews.push({
+        //         id: i + 1,
+        //         "depAirport": "DOH",
+        //         "arrAirport": "JFK",
+        //         airline: airline.name,
+        //         date,
+        //         title: faker.lorem.sentence(),
+        //         text: faker.lorem.lines(randomNum),
+        //         rating: Math.floor(Math.random() * 6)
+        //     })
+            
+        // }
 
         airline.reviews = reviews
 
